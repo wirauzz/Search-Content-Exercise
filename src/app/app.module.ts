77e75be7d6bc5f13325a353fb944ce7179586fe2
Wilson Ramirez Sandy <wramirez995@gmail.com>
2021-08-25 18:22:01 -0400

    Shows the songs by trackName criteria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SongListComponent } from './components/song-list/song-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SongListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/song-list/song-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { SongService } from 'src/app/services/song.service';
+import { Song } from 'src/app/models/song';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-song-list',
+  templateUrl: './song-list.component.html',
+  styleUrls: ['./song-list.component.css']
+})
+export class SongListComponent implements OnInit {
+
+  songs:Song[];
+  songNameControl = new FormControl('')
+  
+  constructor(private songService:SongService) { }
+
+  ngOnInit(): void {
+  }
+
+  searchSong(){
+    this.songService.searchSong(this.songNameControl.value).subscribe(songs =>{
+      this.songs = songs.results
+    })
+  }
+
+}
